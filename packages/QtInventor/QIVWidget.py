0b41cb8d93594c1e58630301112114b9bc965dc5
TheHubbit <TheHubbit@nospam.org>
2014-04-21 19:05:39 +0200

    Added toImage() function.

--- packages/QtInventor/QIVWidget.py
+++ packages/QtInventor/QIVWidget.py
@@ -25,33 +25,62 @@ class QIVWidget(QtOpenGL.QGLWidget):
         self.sizeHintValue = QtCore.QSize(512, 512)
         self.sceneManager = inventor.SceneManager()
         self.sceneManager.redisplay = self.updateGL
+        self.resizeGLWidth = 512
+        self.resizeGLHeight = 512
         # timer must be started from QThread
         self.idleTimer.start()
 
     def initializeGL(self):
+        """Performs initial OpenGL setup"""
         GL.glEnable(GL.GL_DEPTH_TEST)
     
     def paintGL(self):
+        """Renders scene"""
         self.sceneManager.render()
     
     def resizeGL(self, width, height):
+        """Resizes viewport of scene manager"""
         self.sceneManager.resize(width, height)
+        self.resizeGLWidth = width
+        self.resizeGLHeight = height
     
     def mousePressEvent(self, event):
+        """Forwards mouse button press event to scene for processing"""
         self.sceneManager.mouse_button(self.qtButtonIndex.index(event.button()), 0, event.x(), event.y())
     
     def mouseReleaseEvent(self, event):
+        """Forwards mouse button release event to scene for processing"""
         self.sceneManager.mouse_button(self.qtButtonIndex.index(event.button()), 1, event.x(), event.y())
     
     def mouseMoveEvent(self, event):
+        """Forwards mouse move event to scene for processing"""
         self.sceneManager.mouse_move(event.x(), event.y())
 
     def sizeHint(self):
+        """Returns hint for default widget size"""
         return self.sizeHintValue
 
     def setSizeHint(self, size):
+        """Sets hint for default widget size"""
         self.sizeHintValue = QtCore.QSize(size)
 
     def setSizeHint(self, width, height):
+        """Sets hint for default widget size"""
         self.sizeHintValue = QtCore.QSize(width, height)
+
+    def toImage(self, width, height):
+        """Renders the scene into an offline buffer and returns is as QImage instance"""
+        self.makeCurrent()
+        fbo = QtOpenGL.QGLFramebufferObject(width, height, QtOpenGL.QGLFramebufferObject.CombinedDepthStencil)
+        fbo.bind()
+        self.initializeGL()
+        self.sceneManager.resize(width, height)
+        self.sceneManager.render()
+        fbo.release()
+        
+        # restore original viewport size
+        self.sceneManager.resize(self.resizeGLWidth, self.resizeGLHeight)
+
+        return fbo.toImage()
+
 
