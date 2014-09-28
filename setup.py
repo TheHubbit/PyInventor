d97655f98b0b0cdeb27b138c343f3db6155df4a7
TheHubbit <TheHubbit@nospam.org>
2014-09-28 16:07:42 +0200

    Added '--static' option and support for COINDIR environment variable.

--- setup.py
+++ setup.py
@@ -3,9 +3,36 @@ from distutils.sysconfig import *
 import os
 
 # Open Inventor paths and libraries:
+
+# OS X paths by default
 oivincpath = '/Library/Frameworks/Inventor.framework/Resources/include'
 oivlibpath = '/Library/Frameworks/Inventor.framework/Libraries'
-oivlibs = [ 'Coin' ]
+oivlibname = 'Coin'
+
+# Check for "--static" option (Coin only)
+if "--static" in sys.argv:
+    sys.argv.remove("--static")
+    libsuffix = "s"
+    oivcompileargs = ['-DCOIN_NOT_DLL']
+    oivlibs = ['simage1s']
+else:
+    libsuffix = ""
+    oivcompileargs = ['-DCOIN_DLL']
+    oivlibs = [ ]
+
+# Coin environment variable?
+if os.environ.get('COINDIR') is not None:
+    oivincpath = os.environ.get('COINDIR') + '/include'
+    oivlibpath = os.environ.get('COINDIR') + '/lib'
+    if os.environ.get('windir') is not None:
+        # Coin on Windows uses Coin4 rather than just Coin as library name
+        oivlibname = oivlibname + '4'
+
+# Additional libs
+oivlibs += [ oivlibname + libsuffix ]
+if os.environ.get('windir') is not None:
+    oivlibs += [ 'Opengl32', 'Gdi32', 'User32', 'Gdiplus' ]
+
 
 # numpy paths are generated automatically:
 numarrayinc = os.path.join(get_python_lib(plat_specific=1), 'numpy/numarray/include')
@@ -15,6 +42,7 @@ module1 = Extension('inventor',
                     include_dirs = [numarrayinc, numcoreinc, oivincpath],
                     libraries = oivlibs,
                     library_dirs = [oivlibpath],
+                    extra_compile_args = oivcompileargs,
                     sources = ['src/PyInventor.cpp', 
                                'src/PySceneManager.cpp', 
                                'src/PySceneObject.cpp', 
@@ -25,7 +53,7 @@ setup (name = 'PyInventor',
        description = 'Python integration for Open Inventor toolkit',
        author = 'Thomas Moeller',
        author_email = '',
-       url = '',
+       url = 'http://thehubbit.github.io/PyInventor/',
        long_description = '''Python integration for Open Inventor toolkit.''',
        ext_modules = [module1],
        package_dir={'PyInventor': 'packages'},
