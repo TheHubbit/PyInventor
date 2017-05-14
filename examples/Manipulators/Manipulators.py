bdc6c1bb060167f6fff039408f198710fb665229
TheHubbit <TheHubbit@nospam.org>
2017-05-14 11:39:13 -0700

    Initialize graph after show (driver bug?)

--- examples/Manipulators/Manipulators.py
+++ examples/Manipulators/Manipulators.py
@@ -11,24 +11,24 @@ class ApplicationWidget(QtGui.QWidget):
     """Main application window"""
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
-        
-        # create an inventor widget for rendering and a manipulator
-        self.ivWidget = QtInventor.QIVWidget()
-        self.ivWidget.sceneManager.scene = iv.read(__file__.replace(".py", ".iv"))
-        self.manip = iv.TransformBoxManip()
-
-        # find selection node and attach callbacks
-        selectionNode = iv.search(self.ivWidget.sceneManager.scene, type="Selection")[-1]
-        self.selectionSensor = iv.Sensor(selectionNode, "selection", self.selectionCB)
-        self.deselectionSensor = iv.Sensor(selectionNode, "deselection", self.deselectionCB)
 
         # create main application layout
         mainLayout = QtGui.QHBoxLayout()
         mainLayout.setContentsMargins(0, 0, 0, 0)
+        self.ivWidget = QtInventor.QIVWidget()
         mainLayout.addWidget(self.ivWidget)
         self.setLayout(mainLayout)
         self.setWindowTitle(self.tr("Manipulators"))
+
+    def createScene(self):
+        self.ivWidget.sceneManager.scene = iv.read(__file__.replace(".py", ".iv"))
+        self.manip = iv.TransformBoxManip()
         
+        # find selection node and attach callbacks
+        selectionNode = iv.search(self.ivWidget.sceneManager.scene, type="Selection")[-1]
+        self.selectionSensor = iv.Sensor(selectionNode, "selection", self.selectionCB)
+        self.deselectionSensor = iv.Sensor(selectionNode, "deselection", self.deselectionCB)
+    
     def selectionCB(self, path):
         # attach a manipulator to shapes
         if path[-1].get_type() in ['Cone', 'Sphere', 'Cube']:
@@ -43,5 +43,5 @@ if __name__ == '__main__':
     app = QtGui.QApplication([])
     window = ApplicationWidget()
     window.show()
+    window.createScene()
     app.exec_()
-
